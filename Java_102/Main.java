7755ab52b4edfb4ce327e701166897dc8d04eb3a
Quaid01 <qz2022m@outlook.com>
2025-07-24 15:29:08 -0400

    description of what you did

--- Java_102/Main.java
+++ Java_102/Main.java
@@ -1,17 +1,14 @@
 package Java_102;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        Point point = new Point(4, 3);
-        System.out.println("x: " + point.x + " y: " + point.y);
-        Point xTranslation = point.translateX(3); // should be (7, 3)
-        Point yTranslation = point.translateY(-7); // should be (4, -4)
-        Point xyTranslation = point.translateX(-4).translateY(-3); // should be (0, 0) 
-        System.out.println("x transl: " + xTranslation);
-        System.out.println("y transl: " + yTranslation);
-        System.out.println("xy transl: " + xyTranslation);  
-        
-        System.out.println(Point.distance(point, xyTranslation));
+        Grid<Integer> grid = new Grid<>(5, 0);
+        grid.set(2, 2, 4);
+        grid.set(3, 3, 4);
+        System.out.println(grid);
+        System.out.println(grid.diagonal());
     }
 }
--- Java_102/Point.java
+++ Java_102/Point.java
@@ -26,4 +26,35 @@ public class Point {
     public static double distance(Point p1, Point p2) {
         return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
     }
+
+    public static Point centerOfMass(Point[] points) {
+        double sumX = 0;
+        double sumY = 0;
+        for (Point point : points) {
+            sumX += point.x;
+            sumY += point.y;
+        }
+        double comX = sumX/points.length;
+        double comY = sumY/points.length;
+        return new Point(comX, comY);
+    }
+
+    public double angle() {
+        double base_angle = Math.atan(this.y/this.x);
+        double result = 0;
+        if (this.x>=0) {
+            result = Math.toDegrees(base_angle);
+        }
+        if (this.x<0) {
+            result = Math.toDegrees(base_angle + Math.PI);
+        }
+        return result;
+    } 
+
+    public Point rotate(double theta) {
+        double newX = x*Math.cos(theta) + x*Math.sin(theta);
+        double newY = -x*Math.sin(theta) + x*Math.cos(theta);
+        return new Point(newX, newY);
+    }
+
 }
